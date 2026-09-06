97c96edba4e4c9e7b3a04086db3534ce9b890593
PrashantWaghmare <prashantwaghmare9552@gmail.com>
2026-09-06 13:00:07 +0530

    RBI 2023 tags applied; IFSCA/PFRDA difficulty applied; stimuli+options exports

---
+++ activate_reg_exams.sql
@@ -0,0 +1,4 @@
+﻿begin;
+update exams set is_active = true where slug in ('ifsca-grade-a','pfrda-grade-a') and is_active = false;
+select slug, name, is_active from exams where slug in ('ifsca-grade-a','pfrda-grade-a','sebi-grade-a','rbi-grade-b') order by slug;
+commit;
---
+++ deactivate_sandbox.sql
@@ -0,0 +1,4 @@
+﻿begin;
+update exams set is_active = false where slug = 'ssc-cgl-legacy-sandbox-do-not-use';
+select slug, name, is_active from exams where slug = 'ssc-cgl-legacy-sandbox-do-not-use';
+commit;
